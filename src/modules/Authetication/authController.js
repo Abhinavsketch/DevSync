eacf9edf5cded2e09051b884780a6dd3ffc8dcc6
Abhinav Chaubey <abhinavchaubey916@gmail.com>
2026-05-13 16:03:34 +0530

    Creating SignUp Logic

--- src/modules/Authetication/authController.js
+++ src/modules/Authetication/authController.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const userModel = require("./authModels.js")
+const bcrypt = require("bcrypt")
 
-const signupController = ((req,res)=>{
-    const data = req.body;
+const signupController =async (req,res)=>{
+    const {name,email,password} = req.body;
+    const isAlreadyRegister = await userModel.findOne({
+        $or:[
+            {name},
+            {email}
+        ]
+    })
+
+    if(isAlreadyRegister){
+        return res.status(400).json({
+            message:"User already exits"
+        })
+    }
+
+    const hashedPassword = await bcrypt.hash(password,10);
+    const user = await userModel.create({
+        name,
+        email,
+        password:hashedPassword
+    })
+    res.status(500).json({
+        message:"user successfull created",
+        user
+    })
     
-})
+}
 
 module.exports = {
     signupController
