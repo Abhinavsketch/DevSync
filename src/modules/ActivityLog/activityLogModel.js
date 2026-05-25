aaa12a0c2af4c6090b22760b8a53a5085214806e
Abhinav Chaubey <abhinavchaubey916@gmail.com>
2026-05-25 13:59:26 +0530

    Activity Log Utilis

---
+++ src/modules/ActivityLog/activityLogModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+
+const activitySchema = new mongoose.Schema({
+    actor:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    project:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Project"
+    },
+    entityType:{
+        type:String,
+        enum:["Task","Project","Team"]
+    },
+    entity:{
+        type:mongoose.Schema.Types.ObjectId,
+    },
+    action:{
+        type:String,
+        enum:["CREATE_TASK","UPDATE_TASK","DELETE_TASK","STATUS_CHANGE","ADD_MEMBER","REMOVE_MEMBER","UPDATE_ROLE"]
+    },
+    message:{
+        type:String
+    },
+    oldValue:{
+        type:Object,
+        default:null
+    },
+    newValue:{
+        type:Object,
+        default:null
+    }
+},{
+    timestamps:true
+})
+
+const Activity = mongoose.model("Activity",activitySchema)
+
+module.exports = Activity
