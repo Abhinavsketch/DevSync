7626860f66d2bdde26cc9bfa99ef988263b952ca
Abhinav Chaubey <abhinavchaubey916@gmail.com>
2026-07-28 21:01:03 +0530

    Completing Invitation API

--- Frontend/src/api/invitationApi.js
+++ Frontend/src/api/invitationApi.js
@@ -7,3 +7,33 @@ export const createInvitation = async(orgId,email)=>{
 
     return response.data
 }
+
+export const acceptInvitation = async(token)=>{
+    const response = await instance.post(`/invitation/accept/${token}`)
+
+    return response.data
+}
+
+export const rejectInvitation = async(token)=>{
+    const response = await instance.post(`/invitation/reject/${token}`)
+
+    return response.data
+}
+
+export const cancelInvitation = async(inviteId)=>{
+    const response = await instance.post(`/invitation/cancel/${inviteId}`)
+
+    return response.data
+}
+
+export const listOrganizationInvites = async (orgId)=>{
+    const response = await instance.get(`/invitation/invites/${orgId}`)
+
+    return response.data
+}
+
+export const listUserInvites = async ()=>{
+    const response = await instance.get(`/invitation/yourinvites`)
+
+    return response.data
+}
